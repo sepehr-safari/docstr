e7260c6976b0a8ac24bd7c99dd1c409a15c19b27
Sepehr Safari <safari.sepehr@gmail.com>
2024-03-16 14:54:11 +0330

    move main layout to pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,25 +6,65 @@ import { HomePage } from './home';
 import { MyPage } from './my';
 import { NewPage } from './new';
 
+import { UserAvatar } from '@/features/user-avatar';
+
+import { Footer } from '@/shared/components/footer';
+import { Navbar } from '@/shared/components/navbar';
+
+// TODO: extract to shared layout
+export const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Navbar>
+        <UserAvatar />
+      </Navbar>
+
+      <div className="px-6 pt-24 pb-12 h-full mx-auto max-w-screen-lg">{children}</div>
+
+      <Footer />
+    </>
+  );
+};
+
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomePage />,
+    element: (
+      <MainLayout>
+        <HomePage />
+      </MainLayout>
+    ),
   },
   {
     path: '/my',
-    element: <MyPage />,
+    element: (
+      <MainLayout>
+        <MyPage />
+      </MainLayout>
+    ),
   },
   {
     path: '/new',
-    element: <NewPage />,
+    element: (
+      <MainLayout>
+        <NewPage />
+      </MainLayout>
+    ),
   },
   {
     path: '/explore',
-    element: <ExplorePage />,
+    element: (
+      <MainLayout>
+        <ExplorePage />
+      </MainLayout>
+    ),
   },
   {
     path: '/doc/:naddr',
-    element: <DocPage />,
+    element: (
+      <MainLayout>
+        <DocPage />
+      </MainLayout>
+    ),
   },
 ]);

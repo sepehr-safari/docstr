fe28940b2df86a12c89aac58a1cc7863754f50b0
Sepehr Safari <safari.sepehr@gmail.com>
2024-07-03 23:28:14 +0330

    update appwide footer

--- src/shared/components/footer/index.tsx
+++ src/shared/components/footer/index.tsx
@@ -1,15 +1,13 @@
-import { Separator } from '@/shared/components/ui/separator';
-
 export const Footer = () => {
   return (
     <div className="fixed z-20 bottom-0 left-0 right-0 px-6 py-2 flex items-center gap-2 border-t bg-primary-foreground">
-      <p className="text-xs text-gray-500">
+      <p className="text-xs text-muted-foreground">
         Made with <span className="text-red-500">💜</span> by Sepehr
       </p>
 
-      <Separator orientation="vertical" />
+      <p className="text-xs text-muted-foreground">|</p>
 
-      <p className="text-xs text-gray-500">
+      <p className="text-xs text-muted-foreground">
         <span>Powered by</span>
         <a
           href="https://osty.dev"

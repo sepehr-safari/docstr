cd9d179c0b25cf49aa39b75aedaca6593cfb1c91
Sepehr Safari <safari.sepehr@gmail.com>
2024-07-04 16:04:45 +0330

    update logo

--- src/shared/components/navbar/index.tsx
+++ src/shared/components/navbar/index.tsx
@@ -1,11 +1,15 @@
+import { FileIcon } from '@radix-ui/react-icons';
+import { Link } from 'react-router-dom';
+
 import { ModeToggle } from '@/shared/components/mode-toggle';
 
 export const Navbar = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="fixed z-20 top-0 left-0 right-0 h-16 flex items-center gap-2 p-6 bg-primary-foreground">
-      <div>
+      <Link to="/" className="flex items-center gap-1">
+        <FileIcon className="w-8 h-8" />
         <h1 className="text-lg font-bold">Docstr</h1>
-      </div>
+      </Link>
 
       <div className="ml-auto flex items-center gap-2">
         <ModeToggle />

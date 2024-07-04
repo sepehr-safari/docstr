eb624fd2971bd6d0119ced06668136985dd71111
Sepehr Safari <safari.sepehr@gmail.com>
2024-07-04 16:07:47 +0330

    add login btn

--- src/features/active-user-avatar/index.tsx
+++ src/features/active-user-avatar/index.tsx
@@ -5,9 +5,14 @@ import { NOSTR_ICON_URL } from '@/shared/config';
 import { loader } from '@/shared/utils';
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/shared/components/ui/avatar';
+import { Button } from '@/shared/components/ui/button';
 
 export const ActiveUserAvatar = () => {
   const { activeUser } = useActiveUser();
+
+  if (!activeUser) {
+    return <Button disabled>Log in</Button>;
+  }
 
   return (
     <Avatar className="w-8 h-8">
